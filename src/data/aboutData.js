09c6dcbf5ef5f5305971aae1d08cb281848f7b5b
Md Sharique Anwer <shariqueanwer002@gmail.com>
2022-07-23 15:37:40 +0530

    about section updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,8 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "Hello! My name is Sharique Anwer. I'm an aspiring web developer based in India.",
-    description2: "I am a student at Masai School, currently learning Fullstack Web-Development. A passionate Full Stack Web Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs / MongoDB / ExpressJS and some other cool libraries and frameworks. I love to learn and explore new arenas.",
-    image: 2
-}
+  title: "Who I am",
+  description1:
+    "Hello! My name is Sharique Anwer. I'm an aspiring web developer based in India.",
+  description2:
+    "A passionate Full Stack Web Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs / MongoDB / ExpressJS and some other cool libraries and frameworks. I strive to build immersive and beautiful web and mobile applications through carefully crafted code and user-centric design.",
+  image: 2,
+};
